0c04b0252e01b8c1cd5cb412799ce42c9b6ffbd0
hipeoplea <73573072+hipeoplea@users.noreply.github.com>
2021-01-26 23:41:21 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("coffee.db")
+        self.select_data()
+
+    def select_data(self):
+        cur = self.connection.cursor()
+        res = cur.execute("""SELECT * FROM sorts""").fetchall()
+        self.tableWidget.setColumnCount(len(res[0]))
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
